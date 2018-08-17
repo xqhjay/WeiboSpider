749160832a3347b3bbc5fcb44769396ef9bcf996
xuqh <xqhjay@foxmail.com>
2018-08-17 12:20:44 +0800

    Initial commit

---
+++ run.py
@@ -0,0 +1,9 @@
+# !/usr/bin/env python
+# !-*-coding:utf-8 -*-
+# !@Time   : 2018/7/10 18:14
+# !@Author : xuqh
+# !@Email  : xqhjay@foxmail.com
+# !@File   : cookies.py
+from scrapy import cmdline
+
+cmdline.execute("scrapy crawl SinaSpider".split(" "))
---
+++ sina/middlewares.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+import random
+from sina.cookies import cookies, init_cookies
+from sina.user_agents import agents
+
+
+class UserAgentMiddleware(object):
+    """ 换User-Agent """
+
+    def process_request(self, request, spider):
+        agent = random.choice(agents)
+        request.headers["User-Agent"] = agent
+
+
+class CookiesMiddleware(object):
+    """ 换Cookie """
+
+    def __init__(self):
+        init_cookies()
+
+    def process_request(self, request, spider):
+        cookie = random.choice(cookies)
+        request.cookies = cookie
+
+
